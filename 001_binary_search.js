2af406dd0cd8031a4ea89139daf62b2a188318d0
Sofi Bel <s_bel3@icloud.com>
2023-06-06 09:16:49 +0700

    update binary search algorithm

---
+++ 001_binary_search.js
@@ -0,0 +1,25 @@
+function binarySearch(list, item) {
+    let low = 0;
+    let high = list.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let guess = list[mid];
+
+        if (guess === item) {
+            return mid;
+        }
+        else if (guess > item) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return null;
+}
+
+const list = [1, 3, 5, 7, 9];
+
+console.log(binarySearch(list, 3))
+console.log(binarySearch(list, -1))
